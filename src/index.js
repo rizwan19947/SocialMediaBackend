225354391eaa9d0dc35e718278d58722b210160d
Rizwan Syed <expectp@gmail.com>
2024-02-21 02:55:33 +0500

    Added utils for responses and helper functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv';
 import connectDB from '../db/index.js';
-import express from 'express';
-
+import { app } from './app.js';
 dotenv.config({
   path: './env'
 });
 
-const app = express();
+const port = process.env.PORT || 8000;
 
-connectDB();
+connectDB().then(() => {
+  app.on('error', (error) => {
+    console.log("ERROR: ", error);
+    throw error;
+  })
+  app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+  })
+}).catch((err) => {
+  console.log('MONGO db connection failed !!!', err);
+});
