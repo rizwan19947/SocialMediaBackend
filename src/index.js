efd360d755b026365a5b8105d225b9e7fa195f6c
Rizwan Syed <expectp@gmail.com>
2024-02-29 17:09:19 +0500

    Set up jwt tokens and user/video models

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from '../db/index.js';
 import { app } from './app.js';
+
 dotenv.config({
-  path: './env'
+  path: './env',
 });
 
 const port = process.env.PORT || 8000;
 
 connectDB().then(() => {
   app.on('error', (error) => {
-    console.log("ERROR: ", error);
+    console.log('ERROR: ', error);
     throw error;
-  })
+  });
   app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
-  })
+  });
 }).catch((err) => {
   console.log('MONGO db connection failed !!!', err);
 });
