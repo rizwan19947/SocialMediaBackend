57505e1b21bfc29c55be71fa01e1b6ce90da1fa9
Rizwan <expectp@gmail.com>
2024-04-04 02:52:38 +0500

    Update user avatar handler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -273,6 +273,20 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, user, 'Account details updated successfully!'));
 });
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, 'Avatar file is missing!');
+    }
+
+    const avatar = uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar.url) {
+        throw new ApiError(400, 'Error while uploading avatar on cloudinary');
+    }
+});
+
 export {
     registerUser,
     loginUser,
