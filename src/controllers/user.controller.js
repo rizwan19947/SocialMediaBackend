8e60467a5ae712cdaa74add39967fe43f07c3547
Rizwan <expectp@gmail.com>
2024-04-04 06:06:48 +0500

    Aggregation pipeline for get user channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -327,6 +327,98 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, user, 'Cover image updated successfully!'));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, 'Profile username is missing!');
+    }
+
+    const channel = User.aggregate([
+        {
+            /**
+             * Match the user
+             */
+            $match: {
+                username: username?.toLowerCase(),
+            },
+        },
+        {
+            /**
+             * 1st Pipeline
+             * Check the subscribers count for the user
+             */
+            $lookup: {
+                /**
+                 * Check from subscription model for the model name
+                 * mongodb will turn it completely to lowercase
+                 * and add an 's' at the end of the name
+                 */
+                from: 'subscriptions',
+                localField: '_id',
+                foreignField: 'channel',
+                as: 'subscribers',
+            },
+        },
+        {
+            /**
+             * 2nd Pipeline
+             * Check the user's subscribed to channels
+             */
+            $lookup: {
+                from: 'subscriptions',
+                localField: '_id',
+                foreignField: 'subscriber',
+                as: 'subscribedTo',
+            },
+        },
+        {
+            /**
+             * 3rd Pipeline
+             */
+            $addFields: {
+                subscribersCount: {
+                    $size: '$subscribers',
+                },
+                channelsSubscribedToCount: {
+                    $size: '$subscribedTo',
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, '$subscribers.subscriber'] },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+        {
+            /**
+             * 4th Pipeline
+             * Limiting the number of fields being returned
+             * Getting rid of password, created and updated at and some other
+             * unneeded fields
+             */
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            },
+        },
+    ]);
+
+    if (!channel?.length) {
+        throw new ApiError(404, 'Channel does not exist!');
+    }
+
+    return res.status(200).json(new ApiResponse(200, channel[0], 'User channel fetched successfully!'));
+});
+
 
 export {
     registerUser,
@@ -338,4 +430,5 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    getUserChannelProfile,
 };
